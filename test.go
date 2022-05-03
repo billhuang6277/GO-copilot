9180a6db5a53847e03015674bbb757b77eac1c58
qinghua <18682136277@qq.com>
2022-05-03 12:44:50 +0800

    git add .

---
+++ test.go
@@ -0,0 +1,15 @@
+// You can edit this code!
+// Click here and start typing.
+package main
+
+import "fmt"
+
+func main() {
+	fmt.Println("Hello, 世界")
+	fmt.Println("hello, GO")
+	fmt.Println("这是我一次写GO代码")
+	fmt.Println("这是git测试代码")
+	fmt.Println("这是我第二次写GO代码")
+}
+
+//
